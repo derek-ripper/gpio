10b922e72739c1935c4658e741c66c9a5c1e8639
DAR <derek@firstcottage.com>
2019-11-21 12:07:16 +0000

    first load from PC for humid class

---
+++ humidity/snippets/humid2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import Adafruit_DHT
+
+
+
+
+class humidity(object):
+
+    def read_ht(self,DHT_SENSOR,DHT_PIN):
+        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        return humidity, temperature
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    DHT_SENSOR = Adafruit_DHT.DHT22
+    DHT_PIN    = 4
+    
+    while True:
+        humid, temp = humidity(DHT_SENSOR,DHT_PIN)
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+
+    except KeyboardInterrupt:
+        print("\nUser abort with CTRL-C\n")
+        
+        
+        
+# while True:
+    # humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+    # if humidity is not None and temperature is not None:
+        # print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    # else:
+        # print("Failed to retrieve data from humidity sensor")
