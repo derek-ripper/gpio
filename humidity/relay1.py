b6fc41d4aa0371d7ece8caaafb74698c1f00a656
Derek Ripper <derek@firstcottage.com>
2020-01-12 17:51:43 +0000

    backup  + mods for reading config to added

--- humidity/relay1.py
+++ humidity/relay1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''
+''' 
 **********************************************************************
 * Filename      : relay1.py
 * Description           : a sample script for 2-Channel High trigger Relay 
@@ -31,16 +31,18 @@ DHT_PIN    = 4
 o_HT = sensor.humidity(DHT_SENSOR,DHT_PIN)
 o_T  = T.timer()
 
-
 def main():
-    ### CONSTANTS
-    MINS2SECS     = 60           # Normally=60, set to 1 for Testing 
-    InitialOnTime = 20           # seconds
-    PollTime      =  5*MINS2SECS # seconds
-    RestTime      = 10*MINS2SECS # seconds
-    MaxConRunTime = 45*MINS2SECS # seconds
-    OffThres = 39                # % RH - Threshold to turn OFF power
-    OnThres  = 40                # % RH - Threshold to turn ON  power
+    import config   as CFG    
+    o_cfg     = CFG.ReadConfig('config.txt')
+    
+    ### CONSTANTS 
+    InitialOnTime = int(o_cfg.GetConfigValue('INITIALONTIME'))
+    PollTime      = int(o_cfg.GetConfigValue('POLLTIME'))
+    RestTime      = int(o_cfg.GetConfigValue('RESTTIME'))
+    MaxConRunTime = int(o_cfg.GetConfigValue('MAXCONRUNTIME'))
+    
+    OffThres = float(o_cfg.GetConfigValue('OFFTHRES')) # % RH - Threshold to turn OFF power
+    OnThres  = float(o_cfg.GetConfigValue('ONTHRES'))  # % RH - Threshold to turn ON  power
     
     ### COUNTERS
     OFFcount = 0
@@ -55,6 +57,13 @@ def main():
     sleep(InitialOnTime)
     o_T.resetstarttime()
     
+    ### write basic data to log file
+    logger.write("OFF threshold: "+str(OffThres)+" %RH")
+    logger.write("ON  threshold: "+str(OnThres) +" %RH")
+    logger.write("Poll interval: "+str(PollTime)+" secs")
+    logger.write("Max run tme  : "+str(MaxConRunTime)+" secs")
+    logger.write("Rest Time    : "+str(RestTime)+" secs")  
+       
     ### THE POLLING LOOP  ###
     while True: 
         PollCount += 1
@@ -74,10 +83,10 @@ def main():
                 o_RL1.switchON()
             else:          # 2nd or subsequent time in this code section
                 logger.write("State = SWITCHed  ON")
-                CheckMaxRunTime(MaxConRunTime, RestTime)            
+                CheckMaxRunTime(MaxConRunTime, RestTime, ON_FLAG)            
         
         elif hvalue <= OffThres:
-            if ON_FLAG : # ie 1st time inhis code section
+            if ON_FLAG : # ie 1st time in this code section
                 ON_FLAG = False
                 OFFcount += 1                
                 logger.write("SWITCHing OFF,count= "+str(OFFcount))
@@ -87,20 +96,25 @@ def main():
             else:
                 logger.write("State = SWITCHed OFF")
     
-        else:
-            logger.write("In the MAX to MIN zone ON_FLAG = "+str(ON_FLAG))
+        else: # Between the Max and Min thresholds
             if ON_FLAG:
-                CheckMaxRunTime(MaxConRunTime, RestTime)
+                logger.write("State = SWITCHed ON  - In the MAX to MIN zone")
+                CheckMaxRunTime(MaxConRunTime, RestTime,ON_FLAG )
+            else:
+                logger.write("State = SWITCHed OFF - In the MAX to MIN zone")
 
         logger.write("END OF POLLING LOOP")             
         sleep(PollTime)
         # End of Polling Loop
         
-def CheckMaxRunTime(MaxConRunTime, RestTime):
+def CheckMaxRunTime(MaxConRunTime, RestTime, ON_FLAG):
         if  o_T.elapsedtime() > MaxConRunTime:
             logger.write("MAX Continuous runing time reached. Value = "+str(round(o_T.elapsedtime(),2)) )
-            logger.write("Invoking rest time of: "+str(RestTime)) 
+            logger.write("Invoking rest time of: "+str(RestTime))
+            ON_FLAG = False 
+            o_RL1.switchOFF()
             o_T.calctotruntime()
+            
             sleep(RestTime)
         else:
             logger.write("Max run time NOT exceeded")
