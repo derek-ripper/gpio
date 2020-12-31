1511b4d39220c4ae260333bea5279ee18411dd1a
Derek_Pi3 <derek@firstcottage.com>
2020-12-31 17:42:48 +0000

    1st load of LCD contrls/w & test program humid2.py

---
+++ RPLCD_class.py
@@ -0,0 +1,84 @@
+# **********************************************************************
+# * Filename      : RPLCD_class.py
+# * Purpose       : Using std lib for LCD display with extra functions 
+#                 : to extand the class  to class
+#
+# * Created       : 31 Dec 201920 fully reworked in Oct 2020 
+# * Author        : Derek
+# **********************************************************************
+# updates:
+# **********************************************************************
+'''
+usage: 
+from  RPLCD_class import Mylcd
+
+lcd = Mylcd(i2c_expander='PCF8574', address=0x27)
+    # params i2c_expander and address of display are mandatory
+    #
+    # optional other  params that are by default as follows:
+    
+	# cols=20, rows=4, dotsize=8,
+	# charmap='A02',
+	# auto_linebreaks=True,
+	# backlight_enabled=True,
+	# port = 1) # port = 0 on older Pi's
+'''
+from RPLCD.i2c import CharLCD
+import time
+
+
+class Mylcd(CharLCD):
+	def __init__(self,   i2c_expander='PCF8574', address=0x27):
+		super().__init__(i2c_expander='PCF8574', address=0x27)
+		
+		# generate special characters
+		self.genchars()
+				
+	def write2pos(self,value, line=1, pos=1):
+		#adjust screen origin to 0,0 at Top lefthand corner
+		line -= 1
+		pos  -= 1 
+		self.cursor_pos = (line, pos)
+		self.write_string(value)
+		
+	def cleanup(self):
+		self.close(clear=True)
+		self.backlight_enabled = False
+		
+	def on(self):
+		self.display_enabled = True
+	
+	def off(self):
+		self.display_enabled = False
+		
+	def cls(self):
+		self.clear()
+		
+	# create custom characters
+
+	def genchars(self):
+		#char=0 as Degree symbol
+		degree = [
+		0b00110,
+		0b01001,
+		0b01001,
+		0b00110,
+		0b00000,
+		0b00000,
+		0b00000,
+		0b00000,
+		]
+		#char=1 as a box
+		box = [
+		0b11111,
+		0b10001,
+		0b10001,
+		0b10001,
+		0b10001,
+		0b10001,
+		0b10001,
+		0b11111,
+		]
+		self.create_char(0, degree)
+		self.create_char(1, box)	
+		
--- humidity/humid2.py
+++ humidity/humid2.py
@@ -15,6 +15,8 @@ import time as T
 # needed to access my other Pyton std. modules
 sys.path.append(os.path.abspath("/home/pi/dev/gpio"))
 import DU
+from  RPLCD_class import Mylcd
+
 
 if __name__ == '__main__':
 
@@ -22,16 +24,31 @@ if __name__ == '__main__':
     DHT_PIN    = 4
     DHT        = humidity()
 
+    lcd = Mylcd(i2c_expander='PCF8574', address=0x27)
     
-    logger = DU.c_logger("/home/pi/dev/gpio/humidity/", "logx.txt")
+    logger = DU.c_logger("/home/pi/dev/gpio/humidity/", "logxxx.txt")
+    errcnt = 0
+    polls  = 0
     
+    lcd.write2pos("Bad Read = "+str(errcnt),4,1)   
+
+        
     while True:
         try:
             T.sleep(5)
+            polls += 1
             humid, temp = DHT.read_ht(DHT_SENSOR,DHT_PIN)
-            print(humid)
-            print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temp, humid))
-            logger.write("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temp, humid))
+            if humid != 999:
+                lcd.write2pos("Temp     = {0:0.2f}".format(temp)+chr(0)+"C", 1,1)
+                lcd.write2pos("HR %     = {0:0.2f}".format(humid),2,1)
+                lcd.write2pos("Poll cnt = {0:0.0f}".format(polls),3,1)
+                print(humid)
+                print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temp, humid))
+                logger.write("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temp, humid))
+            else:
+                errcnt += 1
+                logger.write("Read Errors + " +str(errcnt))
+                lcd.write2pos("Bad Read = "+str(errcnt),4,1)   
         
         except KeyboardInterrupt:
             print("\nUser abort with CTRL-C\n")
